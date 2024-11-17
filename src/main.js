c5abd9bd5f922584014602fe576751e33c61ae21
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-11-18 00:37:02 +0200

    update main

--- src/main.js
+++ src/main.js
@@ -5,15 +5,19 @@ const path = require('node:path');
 // const WebSocket = require('websocket');
 const fsp = require('node:fs/promises');
 const loadEnv = require('./loadEnv.js');
+const parseCookie = require('./parseCookie.js');
 const staticServer = require('./services/static');
 const Storage = require('./storage/fileStorage.js');
-const staticControllers = require('./static.js');
+const load = require('./load.js');
+const routing = require('./staticRouting.js');
+const buildRoutes = require('./buildRoutes.js');
 
 const envpath = path.join(__dirname, '.env');
 const staticpath = path.join(__dirname, 'static');
 const keypath = path.join(__dirname, 'cert/key.pem');
 const sessionspath = path.join(__dirname, 'sessions');
 const certpath = path.join(__dirname, 'cert/cert.pem');
+const controllerspath = path.join(__dirname, 'controllers');
 
 const tlsOptions = async (keypath, certpath) => ({
   key: await fsp.readFile(keypath),
@@ -21,11 +25,18 @@ const tlsOptions = async (keypath, certpath) => ({
 });
 
 const main = async () => {
+  const storage = await new Storage(sessionspath);
+  const sandbox = {
+    storage,
+    parseCookie,
+    redirect: (location, status) => ({ status, location }),
+    render: (location, status) => ({ status, location }),
+  };
   const options = await tlsOptions(keypath, certpath);
   const { port } = await loadEnv(envpath);
-  const storage = await new Storage(sessionspath);
-  const controllers = staticControllers(storage);
-  staticServer(options, port, staticpath, controllers);
+  const controllers = await routing('.js')(controllerspath);
+  const routes = await buildRoutes(controllers, (path) => load(path, sandbox));
+  staticServer(options, port, staticpath, routes);
 };
 
 main();
