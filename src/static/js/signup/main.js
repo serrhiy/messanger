2e2b2ada0e77a0c9c8881dba2ced4f94a966e72c
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-11-18 09:56:14 +0200

    add new static files

--- src/static/js/signup/main.js
+++ src/static/js/signup/main.js
@@ -1,9 +1,24 @@
 'use strict';
 
-const fileInput = document.getElementById('file-input');
-const fileInputLable = document.getElementById('file-input-label');
+const api = 'https://127.0.0.1:8080/users';
 
-fileInput.addEventListener('change', () => {
-  const file = fileInput.files[0];
-  fileInputLable.innerText = 'selected';
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const firstName = document.getElementById('firstName');
+const secondName = document.getElementById('secondName');
+const password1 = document.getElementById('password1');
+const password2 = document.getElementById('password2');
+
+const inputs = [username, firstName, secondName, password1, password2];
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const result = {
+    username: username.value,
+    firstName: firstName.value,
+    secondName: secondName.value,
+    password: password1.value
+  };
+  for (const input of inputs) input.value = '';
+  fetch(api, { method: 'post', body: JSON.stringify(result) });
 });
