b5042170798f12ec60bedfc74a58ed25f41c83d7
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-11-18 09:55:22 +0200

    update static folder

--- src/services/static/main.js
+++ src/services/static/main.js
@@ -1,19 +1,28 @@
 'use strict';
 
+const path = require('node:path');
+const loadjs = require('../loadjs.js');
 const http2 = require('node:http2');
 const cacheFile = require('./cacheFile.js');
+const prepareUrl = require('../prepareUrl.js');
+const routing = require('../staticRouting.js');
 const contentType = require('./contentType.js');
-const routing = require('../../staticRouting.js');
-const builtRoutes = require('../../buildRoutes.js');
+const buildRoutes = require('../buildRoutes.js');
+const parseCookie = require('../parseCookie.js');
+const Storage = require('../storage/fileStorage.js');
 
-const prepareUrl = (url) => {
-  const sliced = url.startsWith('/') ? url.slice(1) : url;
-  return sliced.endsWith('/') ? sliced.slice(0, sliced.length - 1) : sliced;
-};
+const sessionspath = path.join(__dirname, 'sessions');
 
-module.exports = async (options, port, staticFolder, controllers) => {
+module.exports = async (options, port, staticFolder, controllerspath) => {
+  const sandbox = {
+    storage: await new Storage(sessionspath),
+    parseCookie: parseCookie,
+    redirect: (location, status) => ({ status, location }),
+    render: (location, status) => ({ status, location }),
+  };
+  const controllers = await loadjs(controllerspath, sandbox);
   const table = await routing(null, null)(staticFolder);
-  const routes = await builtRoutes(table, cacheFile);
+  const routes = await buildRoutes(table, cacheFile);
   const types = contentType(table);
   const server = http2.createSecureServer(options);
   const notFound = routes.get('404.html');
@@ -32,7 +41,7 @@ module.exports = async (options, port, staticFolder, controllers) => {
       });
       return void stream.end(buffer);
     }
-    const controller = controllers.get(url); 
+    const controller = controllers.get(url);
     const { status, location } = await controller(headers.cookie);
     if (status >= 300 && status < 400) {
       stream.respond({ location, ':status': status });
@@ -40,9 +49,9 @@ module.exports = async (options, port, staticFolder, controllers) => {
     }
     const buffer = routes.get(location);
     stream.respond({
+      ':status': 200,
       'content-type': types.get(buffer),
       'content-encoding': 'gzip',
-      ':status': 200,
     });
     stream.end(buffer);
   });
