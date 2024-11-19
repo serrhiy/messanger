12e6e37a8e6ba4f5059b7af6a988902927f2afb6
Lytvynenko Serhiy <serrheylitvinenko@gmail.com>
2024-11-19 10:23:20 +0200

    add checks ishtml check

--- src/services/static/main.js
+++ src/services/static/main.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const path = require('node:path');
 const http2 = require('node:http2');
 const cacheFile = require('./cacheFile.js');
 const prepareUrl = require('../prepareUrl.js');
 const routing = require('../staticRouting.js');
 const contentType = require('./contentType.js');
 const buildRoutes = require('../buildRoutes.js');
+
+const ishtml = (url) => path.extname(url) === '';
 
 module.exports = async (options, port, staticFolder, proxy) => {
   const table = await routing('.html')(staticFolder);
@@ -15,7 +18,7 @@ module.exports = async (options, port, staticFolder, proxy) => {
   const notFound = routes.get('404.html');
   server.on('stream', async (stream, headers) => {
     const url = prepareUrl(headers[':path']);
-    if (!url.includes('.')) {
+    if (ishtml(url)) {
       const answer = await proxy(url, headers);
       if (!answer.success) {
         stream.respond({ ':status': 302, location: answer.path });
